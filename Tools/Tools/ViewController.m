6e13b7a679dc69d35d68f2be382135a87c77c995
lwq <1017326372@qq.com>
2018-06-06 09:26:36 +0800

    增加版本更新工具

--- Tools/Tools/ViewController.m
+++ Tools/Tools/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "JJVersionTool.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [JJVersionTool updataVersion];
+
+ 
+
 }
 
 
